1d36234e67c2d4a25e9a4d7a136437c2c7d53e07
Jngvarr <jngvarr@inbox.ru>
2022-10-19 13:14:22 +0700

    Решена четвертая задача

---
+++ lesson1/homework/4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число >");
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+Console.Write($"Четные числа от 1 до {number}: ");
+while (count <= number)
+{
+    if (count % 2 == 0) Console.Write(count + " ");
+    count++;
+}
