a3853941a707438c6ed45e89f9694daa9b28e0c8
Jngvarr <jngvarr@inbox.ru>
2022-10-19 12:00:44 +0700

    домашка, сделано две задачи

---
+++ lesson1/homework/2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Определение максимального из трех чисел");
+Console.Write("Введите первое число > ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число > ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число > ");
+int number3 = int.Parse(Console.ReadLine());
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"{number1} максимальное из чисел {number1}, {number2}, {number3}");
+}
+else if (number2 > number3 && number2 > number1)
+{
+    Console.WriteLine($"{number2} максимальное из чисел {number1}, {number2}, {number3}");
+}
+else
+{
+    Console.WriteLine($"{number3} максимальное из чисел {number1}, {number2}, {number3}");
+}
