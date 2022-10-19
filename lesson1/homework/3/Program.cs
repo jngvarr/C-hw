23e4a27846594f162c7cc3d7accb482ccc8927d6
Jngvarr <jngvarr@inbox.ru>
2022-10-19 12:40:13 +0700

    решена третья задача

---
+++ lesson1/homework/3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+
+
+Console.WriteLine("Проверка числа на четность");
+Console.Write("Введите число > ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Введенное число {number} - четное");
+}
+else Console.WriteLine($"Введенное число {number} - нечетное");
