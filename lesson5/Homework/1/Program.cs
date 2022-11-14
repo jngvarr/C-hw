9aeeade982167deb356cc46218504b296c09d2f7
Jngvarr <jngvarr@gmail.com>
2022-11-14 17:19:18 +0700

    add homework

--- lesson5/Homework/1/Program.cs
+++ lesson5/Homework/1/Program.cs
@@ -3,3 +3,35 @@
 
 // [345, 897, 568, 234] -> 2
 
+int[] CreateRandomArr()
+{
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 999);
+    }
+    return arr;
+}
+void PrintArr(int[] arr)
+{
+    Console.Write(arr[0]);
+    for (int i = 1; i < arr.Length; i++)
+    {
+        Console.Write($", {arr[i]}");
+    }
+}
+
+int EvenNumb(int[] arr)
+{
+    int count = 0;
+    foreach (var item in arr)
+    {
+        if (item % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] arr = CreateRandomArr();
+PrintArr(arr);
+Console.WriteLine();
+Console.Write($"Количество четных элементов в массиве {EvenNumb(arr)}");
---
+++ lesson5/Homework/2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateRandomArr()
+{
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-10, 20);
+    }
+    return arr;
+}
+void PrintArr(int[] arr)
+{
+    Console.Write(arr[0]);
+    for (int i = 1; i < arr.Length; i++)
+    {
+        Console.Write($", {arr[i]}");
+    }
+}
+
+int OddNumbSumm(int[] arr)
+{
+    int summ = 0;
+    for (int i = 0; i < arr.Length; i+=2)
+    {
+        summ+=arr[i];
+    }
+    return summ;
+}
+
+int[] arr = CreateRandomArr();
+PrintArr(arr);
+Console.WriteLine();
+Console.Write($"Сумма элементов на нечетных позициях в массиве -> {OddNumbSumm(arr)}");
